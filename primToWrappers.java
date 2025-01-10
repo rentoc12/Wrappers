fc2757f6a3bfc180d3d370cc8aeb129f607c3eb0
Renato Silva <Renatoef79@gmail.com>
2025-01-10 13:03:48 -0300

    first commit

---
+++ primToWrappers.java
@@ -0,0 +1,14 @@
+public class primToWrappers {
+
+
+    public static void main(String[] args) {
+        
+        Integer num1 = 10;
+        System.out.println(num1);
+
+        Double numDouble = 32.3;
+        System.out.println(numDouble);
+    }
+
+
+}
